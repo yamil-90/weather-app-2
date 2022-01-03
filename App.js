93f96c57de22bbc4f807dbbb7b48150bdfb26c4f
yamil-90 <yamil.coloma90@gmail.com>
2022-01-03 01:01:25 -0300

    made the request function and some styles

--- App.js
+++ App.js
@@ -1,38 +1,87 @@
-import React from 'react';
-import {Text, View, StyleSheet, Pressable, Keyboard} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Pressable, Keyboard, Alert } from 'react-native';
+import axios from 'axios';
 
-import Form from './src/components/Form'
 
-const hideKeyBoard = ()=>{
+import Form from './src/components/Form';
+
+const hideKeyBoard = () => {
     Keyboard.dismiss();
 }
 
-const App =()=>{
-return(
-    <>
-<Pressable  onPress={()=>{
-    hideKeyBoard()
-}}>
-<View style={styles.app}>
-    <View style={styles.content}>
-        <Form/>
-    </View>
-</View>
-</Pressable>
+const App = () => {
 
-    </>
-)
+    const [search, setSearch] = useState({
+        country: '',
+        city: ''
+    })
+    const [consultApi, setConsultApi] = useState(false);
+    const { country, city } = search;
+    const [apiResult, setApiResult] = useState('')
+    useEffect(() => {
+        const requestApi = () => {
+            if (consultApi) {
+                    console.log('so?');
+                    const weatherEnabler = "3f40706fa65396a8e1ae8be95faad4c1"
+                    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${weatherEnabler}`
+                    axios.get(url)                    
+                    .then((result)=>{
+                        setApiResult(result.data);
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                        showAlert()
+                    })
+                    
+                
+            }
+            setConsultApi(false)
+        }
+        requestApi()
+    }, [consultApi])
+
+    const showAlert = () => {
+        Alert.alert(
+            'Error',
+            'request error',
+            [
+                { text: 'Ok' }
+            ]
+        )
+    }
+
+    return (
+        <>
+            <Pressable
+                style={styles.app}
+                onPress={() => {
+                    hideKeyBoard()
+                }}>
+                <View>
+                    <View style={styles.content}>
+                        <Form
+                            setSearch={setSearch}
+                            search={search}
+                            setConsultApi={setConsultApi}
+                        />
+                    </View>
+                </View>
+            </Pressable>
+
+        </>
+    )
 };
 
 weatherEnabler = "3f40706fa65396a8e1ae8be95faad4c1"
 mapEnabler = "AIzaSyA-A7EI0w6HlycpbiuJTduX6Fynl_Sg6wc"
 
 const styles = StyleSheet.create({
-    app:{
-    backgroundColor: 'rgb(71,149,212)',
-    justifyContent: 'center'
+    app: {
+        flex: 1,
+        backgroundColor: 'rgb(71,149,212)',
+        justifyContent: 'center'
     },
-    content:{
+    content: {
         marginHorizontal: '2.5%'
     }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,69 @@
-import React from 'react';
-import {Text, View, StyleSheet, TextInput, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Pressable, Animated, Alert } from 'react-native';
 // install react native picker
-import Picker from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 
-const Form =()=>{
+const Form = ({ search, setSearch, setConsultApi }) => {
+    const { country, city } = search;
+
+    const [animationBtn] = useState(new Animated.Value(1))
+
+const requestWeather = ()=>{
+    if(country.trim()==='' || city.trim()===''){
+        showAlert()
+        return
+    }
+    setConsultApi(true)
+}
+const showAlert = ()=>{
+    Alert.alert(
+        'Error',
+        'All the fields are mandatory',
+        [
+            {text: 'Ok'}
+        ]
+    )
+}
+
+    const inAnimation = () => {
+        console.log('in');
+        Animated.spring(animationBtn, {
+            toValue: 0.9,
+            useNativeDriver: false
+        }).start();
+    }
+    const outAnimation = () => {
+        console.log('uot');
+        Animated.spring(animationBtn, {
+            toValue: 1,
+            friction: 4,
+            tension: 30,
+            useNativeDriver: false
+        }).start();
+    }
+
+    const animationStyle = {
+        transform: [{ scale: animationBtn }]
+    }
+   
     return (
         <View style={styles.form}>
             <View>
-                <Text>hola form</Text>
                 <TextInput
-                style={styles.input}
+                    value={city}
+                    onChangeText={cityText => setSearch({ ...search, city:cityText })}
+                    style={styles.input}
                     placeholder='city'
                     placeholderTextColor='#666'
                 />
             </View>
             <View>
                 <Picker
-                itemStyle={{height:120, backgroundColor:'#fff'}}>
+                    onValueChange={(countryText) => (
+                        setSearch({ ...search, country:countryText })
+                    )}
+                    selectedValue={country}
+                    style={{ height: 50, backgroundColor: '#fff' }}>
                     <Picker.Item label='-- select --' value='' />
                     <Picker.Item label='United States' value='USA' />
                     <Picker.Item label='MEXICO' value='MX' />
@@ -24,12 +71,23 @@ const Form =()=>{
                     <Picker.Item label='Costa Rica' value='CR' />
                     <Picker.Item label='Spain' value='ES' />
                     <Picker.Item label='Colombia' value='CO' />
+                    <Picker.Item label='Peru' value='PE' />
                 </Picker>
-                <Pressable>
-                    <View
-                    style={styles.btnSearch}>
+                <Pressable
+                    onPressIn={() => {
+                        inAnimation()
+                    }}
+                    onPressOut={() => {
+                        outAnimation()
+                    }}
+                    onPress={()=>{
+                        requestWeather()
+                    }}
+                >
+                    <Animated.View
+                        style={[styles.btnSearch, animationStyle]}>
                         <Text style={styles.btnText}>search weather</Text>
-                    </View>
+                    </Animated.View>
                 </Pressable>
             </View>
         </View>
@@ -40,28 +98,29 @@ export default Form;
 
 const styles = StyleSheet.create({
 
-form:{
-    marginTop: 100
-},
-input:{
-    padding: 10,
-    height:50,
-    backgroundColor: '#fff',
-    fontSize: 20,
-    marginBottom: 20,
-    texAlign: 'center'
-},
-btnSearch:{
-    marginTop:50,
-    backgroundColor:'#000',
-    padding:10,
-    justifyContent: 'center'
-}, 
-btnText:{
-    color: '#fff',
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    fontWeight: '700',
-    fontSize:18
-}
+    form: {
+        marginTop: 100
+    },
+    input: {
+        padding: 10,
+        height: 50,
+        backgroundColor: '#fff',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    btnSearch: {
+        marginTop: 50,
+        backgroundColor: '#000',
+        padding: 10,
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    btnText: {
+        color: '#fff',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: '700',
+        fontSize: 18
+    }
 })
